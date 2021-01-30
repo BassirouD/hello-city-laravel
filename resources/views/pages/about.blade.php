ff5247be4414e6a42941ebd3da436583162e2629
BassirouD <moubass.diallo@gmail.com>
2021-01-30 18:51:02 +0000

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-    <title>About Us | Hello City</title>
-
-</head>
-<body class="antialiased">
+@section('content')
     <p>Build with &hearts; by BASSIROU DIALLO. </p>
     <p><a href="/">Revenir a la page d'acceuil</a></p>
-    <footer>
-        <!-- Par defaut le timeZone est utc. On peut le modifier
-        dans le fichier app.php qui se trouve dans le rep config-->
-        <p>&copy; Copyright {{ date('Y') }} &middot; </p>
-    </footer>
-</body>
-</html>
+@endsection('content')
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Hello City!</title>
-
-    </head>
-    <body class="antialiased">
+@section('content')
         <h1>Hello from Senegal</h1>
         <p>It's currently {{ date('h:i A') }}.</p>
-        <footer>
-            <!-- Par defaut le timeZone est utc. On peut le modifier
-            dans le fichier app.php qui se trouve dans le rep config-->
-            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
-        </footer>
-    </body>
-</html>
+@endsection('content')

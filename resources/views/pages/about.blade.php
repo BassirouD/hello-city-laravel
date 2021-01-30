0fdfe1352f41cd70fa625fe1047fb097e3d67578
BassirouD <moubass.diallo@gmail.com>
2021-01-30 22:07:50 +0000

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,6 +4,7 @@
 @section('title', 'About Us | ' . env('APP_NAME'))
  <!-- endsection equivant a stop -->
 @section('content')
+    <img src="/images/tdsi.png" alt="TDSI logo">
     <p>Build with &hearts; by BASSIROU DIALLO. </p>
     <p><a href="{{ route('home') }}">Revenir a la page d'acceuil</a></p>
 @endsection

87831029e9dbf70a05700d99cd3117dcf5609d0f
BassirouD <moubass.diallo@gmail.com>
2021-01-30 19:09:18 +0000

    Add proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,9 @@
 @extends('base')
 
+
+@section('title', 'About Us | Hello City')
+ <!-- endsection equivant a stop -->
 @section('content')
     <p>Build with &hearts; by BASSIROU DIALLO. </p>
     <p><a href="/">Revenir a la page d'acceuil</a></p>
-@endsection('content')
+@endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('base')
 
+@section('title')
+    Hello City
+@endsection
 @section('content')
         <h1>Hello from Senegal</h1>
         <p>It's currently {{ date('h:i A') }}.</p>
-@endsection('content')
+@endsection

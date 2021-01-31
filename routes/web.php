63d07c5288cef5dac97b8ef048d1ea1ec8458370
BassirouD <moubass.diallo@gmail.com>
2021-01-31 11:59:39 +0000

    Make use of Route::view instead of Route::get

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::name('home')->get('/', function (){
     return view('pages.welcome');
     // On peut utiliser le point(.) ou la virgule(,) comme separateur
-});
+});*/
 //Les deux facons de nommer nos routes ->name('home')
+
+//Nouveauté php 7 avec Route
+//Route::get('/', fn() => view('page/home'));
+
+//  -----------------------
+Route::view('/', 'pages/welcome')->name('home');
 
 Route::get('/about-us', function (){
     return view('pages/about');
